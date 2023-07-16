e2d5d40fa8da7732d33d20e7089c6c6992b9f30d
Byron <byronochieng01@gmail.com>
2023-07-16 20:42:19 +0300

    route handling file upload

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const multer = require('multer');
+const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -10,6 +12,22 @@ const dbService = require('./dBService');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('../client'));
+
+const storage = multer.diskStorage({
+    destination: './img/',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage
+}).single('file');
+
+app.get('/', (req, res) => {
+    res.sendFile('~/Desktop/mysql-oqay/client/html/index.html');
+})
 
 //create
 app.post('/insert', (req, res) => {
@@ -20,7 +38,7 @@ app.post('/insert', (req, res) => {
 
     result
     .then(data => res.json({ data : data}))
-    .catch(err => console.error(err))
+    .catch(err => console.error(err));
 
 });
 
@@ -67,8 +85,30 @@ app.patch('/update', (req, res) => {
     const result = db.updateRowById(id, product, price, quantity, mass);
 
     result
-    .then(data => res.json({ success : data}))
+    .then(data => res.json({ success : data }))
     .catch(err => console.error(err));
+});
+
+//image upload route
+app.post('/upload', (req, res) => {
+    
+    upload(req, res, (err) => {
+        if (err){
+            console.log("err uploading image");
+        } else {
+            //console.log(req);
+            const { id } = req.body;
+            const filename = req.file.filename;
+
+            const db = dbService.getDbServiceInstance();
+
+            const result = db.addImageById(filename, id);
+
+            result
+            .then(data => res.json({ data : data }))
+            .catch(err => console.error(err));
+        }
+    });
 });
 
 app.listen(process.env.PORT, () => console.log('app is running'));
